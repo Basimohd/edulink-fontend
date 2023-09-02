41e664e200bb39c3ca7fc8e4b8dff93da3841248
Basimohd <basimohammedkt@gmail.com>
2023-09-02 10:40:11 +0530

    fixed err

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production:false,
-    domain: 'https://api.edulinkcollege.online/',
+    domain: 'http://localhost:3000',
     google_client_id: '336368670492-fl6qf7khgck227vn7nrkhn7t6eg4fu4n.apps.googleusercontent.com',
     firebaseConfig : {
         apiKey: "AIzaSyD9N9hhMoMLn7P6_bP1GTEiTCfdwUzQacY",
