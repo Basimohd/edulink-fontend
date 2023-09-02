f5e47c7d790f7efd311edc5f56f2b59a16452679
Basimohd <basimohammedkt@gmail.com>
2023-09-02 10:06:13 +0530

    backend url updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:false,
-    domain: 'http://localhost:3000/',
+    domain: 'api.edulinkcollege.online/',
     google_client_id: '336368670492-fl6qf7khgck227vn7nrkhn7t6eg4fu4n.apps.googleusercontent.com'
 };
